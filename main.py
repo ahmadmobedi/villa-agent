d03532a4afc8b51cfd5df1a3aa45f6f0ae89eabb
ahmad <35064645+ahmadmobedi@users.noreply.github.com>
2026-07-07 07:53:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
 
 TOKEN = os.environ["BOT_TOKEN"]
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "سلام احمد 👋\nربات Villa Agent با موفقیت راه‌اندازی شد."
     )
 
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        """
+دستورات ربات
+
+/start
+شروع ربات
+
+/help
+نمایش راهنما
+"""
+    )
+
+
 def main():
     app = Application.builder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+
     print("Bot is running...")
+
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
